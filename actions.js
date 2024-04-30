5da495399f64f1e0a50296d8d63758226423501c
John A Knight Jr <istnv@istnv.com>
2024-04-29 22:20:05 -0700

    New action to send Hex encoded strings. Allow host names.

--- actions.js
+++ actions.js
@@ -2,7 +2,7 @@ const CHOICES_END = [
 	{ id: '', label: 'None' },
 	{ id: '\n', label: 'LF - \\n (Common UNIX/Mac)' },
 	{ id: '\r\n', label: 'CRLF - \\r\\n (Common Windows)' },
-	{ id: '\r', label: 'CR - \\r (Old MacOS)' },
+	{ id: '\r', label: "CR - \\r (1970's RS232 terminal)" },
 	{ id: '\x00', label: 'NULL - \\x00 (Can happen)' },
 	{ id: '\n\r', label: 'LFCR - \\n\\r (Just stupid)' },
 ]
@@ -16,7 +16,7 @@ export function getActionDefinitions(self) {
 					type: 'textinput',
 					id: 'id_send',
 					label: 'Command:',
-					tooltip: 'Use %hh to insert Hex codes',
+					tooltip: 'Use %hh to insert Hex codes\nObsolete, use Send HEX command instead',
 					default: '',
 					useVariables: true,
 				},
@@ -35,6 +35,57 @@ export function getActionDefinitions(self) {
 					/*
 					 * create a binary buffer pre-encoded 'latin1' (8bit no change bytes)
 					 * sending a string assumes 'utf8' encoding
+					 * which then escapes character values over 0x7F
+					 * and destroys the 'binary' content
+					 */
+					const sendBuf = Buffer.from(cmd + action.options.id_end, 'latin1')
+
+					if (self.config.prot == 'tcp') {
+						self.log('debug', 'sending to ' + self.config.host + ': ' + sendBuf.toString())
+
+						if (self.socket !== undefined && self.socket.isConnected) {
+							self.socket.send(sendBuf)
+						} else {
+							self.log('debug', 'Socket not connected :(')
+						}
+					}
+
+					if (self.config.prot == 'udp') {
+						if (self.udp !== undefined) {
+							self.log('debug', 'sending to ' + self.config.host + ': ' + sendBuf.toString())
+
+							self.udp.send(sendBuf)
+						}
+					}
+				}
+			},
+		},
+		send_hex: {
+			name: 'Send HEX encoded Command',
+			options: [
+				{
+					type: 'textinput',
+					id: 'id_send_hex',
+					label: 'Command:',
+					tooltip: 'Decoding stops at first non-valid hex digit',
+					default: '',
+					useVariables: true,
+				},
+				{
+					type: 'dropdown',
+					id: 'id_end',
+					label: 'Command End Character:',
+					default: '',
+					choices: CHOICES_END,
+				},
+			],
+			callback: async (action) => {
+				const cmd = decode(await self.parseVariablesInString(action.options.id_send_hex), 'hex')
+
+				if (cmd != '') {
+					/*
+					 * create a binary buffer pre-encoded 'latin1' (8bit raw bytes)
+					 * sending a default string assumes 'utf8' encoding
 					 * which then escapes character values over 0x7F
 					 * and destroys the 'binary' content
 					 */
